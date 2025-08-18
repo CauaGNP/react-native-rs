c5a9d119935ca944f2a7586943c6dad27cbe24bd
= <=>
2025-08-17 23:32:12 -0300

    iniciando projeto compras

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { Image, View } from "react-native";
+import { styles } from "./styles";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Entrar" />
+      </View>
+
+      <View style={styles.content}></View>
+    </View>
+  );
+}
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import { Text, TouchableOpacity } from "react-native";
+
+import { styles } from "./styles";
+
+type Props = {
+  title: string;
+};
+export function Button({ title }: Props) {
+  return (
+    <TouchableOpacity style={styles.container} activeOpacity={0.8}>
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
